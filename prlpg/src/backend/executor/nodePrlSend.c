47fb18ed4a19f17fcdb61a2c3e044c7dc5a79f25
danielkov <danielkov@b8b49325-77e1-8e15-c3a2-48206a2107aa>
2010-07-06 20:39:20 +0000

    sending last tuple

--- prlpg/src/backend/executor/nodePrlSend.c
+++ prlpg/src/backend/executor/nodePrlSend.c
@@ -3,20 +3,30 @@
 #include "executor/executor.h"
 #include "executor/nodePrlSend.h"
 #include "storage/parallel.h"
+#include "utils/memutils.h"
 
 TupleTableSlot *ExecPrlSend(PrlSendState *node) {
 	TupleTableSlot * slot;
 	BufferQueue * bq = node->bufferQueue;
-	BufferQueueCell * bqc = (BufferQueueCell *)palloc(sizeof(BufferQueueCell));
+	BufferQueueCell * bqc = NULL;
 	PlanState  *outerNode;
+	MemoryContext oldContext;
+	
 	outerNode = outerPlanState(node);
 	for (;;) {
 		slot = ExecProcNode(outerNode);
 		if (TupIsNull(slot)) {
-			
+			// send last one 
+			oldContext = MemoryContextSwitchTo(ShmParalellContext);
+			// master will deallocate it .. 
+			bqc = (BufferQueueCell *)palloc(sizeof(BufferQueueCell));
+			bqc->last = true;
+			bufferQueueAdd(bq, bqc, false);
+			MemoryContextSwitchTo(oldContext);
+			break;
 		}
 		
-			
+		
 	}
 	
 	return NULL;
