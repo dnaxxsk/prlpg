f3ef785e084d69aab6518ad40a96e684e9b7e82c
danielkov <danielkov@b8b49325-77e1-8e15-c3a2-48206a2107aa>
2010-07-07 21:16:26 +0000

    sending tuples to master

--- trunk/prlpg/src/backend/executor/nodePrlSend.c
+++ trunk/prlpg/src/backend/executor/nodePrlSend.c
@@ -17,7 +17,7 @@ TupleTableSlot *ExecPrlSend(PrlSendState *node) {
 		slot = ExecProcNode(outerNode);
 		if (TupIsNull(slot)) {
 			// send last one 
-			oldContext = MemoryContextSwitchTo(ShmParalellContext);
+			oldContext = MemoryContextSwitchTo(ShmParallelContext);
 			// master will deallocate it .. 
 			bqc = (BufferQueueCell *)palloc(sizeof(BufferQueueCell));
 			bqc->last = true;
@@ -26,6 +26,17 @@ TupleTableSlot *ExecPrlSend(PrlSendState *node) {
 			break;
 		}
 		// TODO send all
+		oldContext = MemoryContextSwitchTo(ShmParallelContext);
+		
+		bqc = (BufferQueue *) palloc(sizeof(BufferQueueCell));
+		bqc->last = false;
+		bqc->ptr_value = (void *)ExecCopySlotMinimalTuple(slot);
+//		MinimalTuple tuple;
+//		tuple = ExecCopySlotMinimalTuple(slot);
+		bufferQueueAdd(bq, bqc, false);
+		// how to get disposed of that "slot" containing the data in a correct way?
+		
+		MemoryContextSwitchTo(oldContext);
 	}
 	
 	return NULL;
